bf30f77df94d40aef9e1df51191ec4ffd522c5f7
Wasif Khan <waasif.khaan@gmail.com>
2018-09-08 00:11:50 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
 
 
+  get '/signup' do
+    if session[:user_id]
+      redirect to "/tweets"
+    else
+    erb :"users/new"
+    end
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to "/signup"
+    else
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    end
+  end
 end
