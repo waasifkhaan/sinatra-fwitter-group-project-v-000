5834ace37596ba7b6423c68698f98409bf8aa44b
Wasif Khan <waasif.khaan@gmail.com>
2018-09-09 20:41:20 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,5 +9,81 @@ get '/tweets' do
   end
 end
 
+get '/tweets/new' do
+  if logged_in?
+    erb:"tweets/create_tweet"
+  else
+    redirect to "/login"
+  end
+end
+
+post '/tweets' do
+  if logged_in?
+    if params[:content] == ""
+      redirect to "tweets/new"
+
+    else
+      @tweet = Tweet.new(content: params[:content])
+      @tweet.user_id = current_user.id
+      current_user.tweets << @tweet
+      if @tweet.save
+        redirect to "tweets/#{@tweet.id}"
+      else
+        redirect to "tweets/new"
+      end
+    end
+  else
+    redirect to "/login"
+  end
+end
+
+get '/tweets/:id' do
+  if logged_in?
+    @tweet = Tweet.find_by(:id => params[:id])
+    erb:"tweets/show"
+  else
+    redirect to "/login"
+  end
+end
+
+get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        erb :'tweets/edit_tweet'
+      else
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+post "/tweets/:id" do
+  if logged_in?
+    if params[:content] == ""
+      redirect to "/tweets/#{params[:id]}/edit"
+    else
+      @tweet = Tweet.find_by(id: params[:id])
+      @tweet.update(content: params[:content])
+
+        redirect to "/tweets/#{@tweet.id}"
+    end
+  else
+    redirect to "/login"
+  end
+end
+
+delete '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        @tweet.delete
+      end
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
 
 end
